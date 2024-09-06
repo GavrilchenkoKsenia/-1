96dd808a26f97bdebce418988054490c9cbd3d8f
Nezhnaya <afinaalfreda@yandex.ru>
2024-09-06 14:16:19 +0300

    Добавьте файлы проекта.

---
+++ FormAddUsers.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace ЛР1
+{
+    partial class FormAddUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ButtonBack = new System.Windows.Forms.Button();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox2 = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.PhonemaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.first_NameTextBox = new System.Windows.Forms.TextBox();
+            this.second_NameTextBox = new System.Windows.Forms.TextBox();
+            this.roleIDComboBox = new System.Windows.Forms.ComboBox();
+            this.radioButtonMen = new System.Windows.Forms.RadioButton();
+            this.radioButtonWomen = new System.Windows.Forms.RadioButton();
+            this.BindingSourceRole = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BindingSourceRole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonBack
+            // 
+            this.ButtonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonBack.Location = new System.Drawing.Point(24, 29);
+            this.ButtonBack.Name = "ButtonBack";
+            this.ButtonBack.Size = new System.Drawing.Size(88, 35);
+            this.ButtonBack.TabIndex = 0;
+            this.ButtonBack.Text = "Назад";
+            this.ButtonBack.UseVisualStyleBackColor = true;
+            this.ButtonBack.Click += new System.EventHandler(this.ButtonBack_Click);
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonAdd.Location = new System.Drawing.Point(648, 389);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(91, 35);
+            this.ButtonAdd.TabIndex = 1;
+            this.ButtonAdd.Text = "Добавить";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(246, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Добавление Нового Пользователя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(424, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(424, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Фамилия:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(424, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Роль:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(430, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Пол:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(63, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Логин:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(63, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Пароль:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(63, 222);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Повтор пароля:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(63, 274);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Эл. почта:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(63, 329);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Телефон:";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(183, 102);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(100, 20);
+            this.loginTextBox.TabIndex = 12;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(183, 164);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 13;
+            // 
+            // passwordTextBox2
+            // 
+            this.passwordTextBox2.Location = new System.Drawing.Point(183, 218);
+            this.passwordTextBox2.Name = "passwordTextBox2";
+            this.passwordTextBox2.PasswordChar = '*';
+            this.passwordTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox2.TabIndex = 14;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(183, 270);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailTextBox.TabIndex = 15;
+            // 
+            // PhonemaskedTextBox
+            // 
+            this.PhonemaskedTextBox.Location = new System.Drawing.Point(183, 329);
+            this.PhonemaskedTextBox.Mask = "+7 (000) 000-00-00";
+            this.PhonemaskedTextBox.Name = "PhonemaskedTextBox";
+            this.PhonemaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PhonemaskedTextBox.TabIndex = 16;
+            // 
+            // first_NameTextBox
+            // 
+            this.first_NameTextBox.Location = new System.Drawing.Point(524, 98);
+            this.first_NameTextBox.Name = "first_NameTextBox";
+            this.first_NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.first_NameTextBox.TabIndex = 17;
+            // 
+            // second_NameTextBox
+            // 
+            this.second_NameTextBox.Location = new System.Drawing.Point(524, 151);
+            this.second_NameTextBox.Name = "second_NameTextBox";
+            this.second_NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.second_NameTextBox.TabIndex = 18;
+            // 
+            // roleIDComboBox
+            // 
+            this.roleIDComboBox.DataSource = this.BindingSourceRole;
+            this.roleIDComboBox.DisplayMember = "Name";
+            this.roleIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.roleIDComboBox.FormattingEnabled = true;
+            this.roleIDComboBox.Location = new System.Drawing.Point(524, 197);
+            this.roleIDComboBox.Name = "roleIDComboBox";
+            this.roleIDComboBox.Size = new System.Drawing.Size(121, 21);
+            this.roleIDComboBox.TabIndex = 19;
+            this.roleIDComboBox.ValueMember = "ID";
+            // 
+            // radioButtonMen
+            // 
+            this.radioButtonMen.AutoSize = true;
+            this.radioButtonMen.Location = new System.Drawing.Point(524, 256);
+            this.radioButtonMen.Name = "radioButtonMen";
+            this.radioButtonMen.Size = new System.Drawing.Size(71, 17);
+            this.radioButtonMen.TabIndex = 20;
+            this.radioButtonMen.Text = "Мужской";
+            this.radioButtonMen.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWomen
+            // 
+            this.radioButtonWomen.AutoSize = true;
+            this.radioButtonWomen.Checked = true;
+            this.radioButtonWomen.Location = new System.Drawing.Point(648, 257);
+            this.radioButtonWomen.Name = "radioButtonWomen";
+            this.radioButtonWomen.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonWomen.TabIndex = 21;
+            this.radioButtonWomen.TabStop = true;
+            this.radioButtonWomen.Text = "Женский";
+            this.radioButtonWomen.UseVisualStyleBackColor = true;
+            // 
+            // BindingSourceRole
+            // 
+            this.BindingSourceRole.DataSource = typeof(ЛР1.DBContext.Roles);
+            // 
+            // FormAddUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonWomen);
+            this.Controls.Add(this.radioButtonMen);
+            this.Controls.Add(this.roleIDComboBox);
+            this.Controls.Add(this.second_NameTextBox);
+            this.Controls.Add(this.first_NameTextBox);
+            this.Controls.Add(this.PhonemaskedTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.passwordTextBox2);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.ButtonBack);
+            this.Name = "FormAddUsers";
+            this.Text = "FormAddUsers";
+            this.Load += new System.EventHandler(this.FormAddUsers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BindingSourceRole)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonBack;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox2;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.MaskedTextBox PhonemaskedTextBox;
+        private System.Windows.Forms.TextBox first_NameTextBox;
+        private System.Windows.Forms.TextBox second_NameTextBox;
+        private System.Windows.Forms.ComboBox roleIDComboBox;
+        private System.Windows.Forms.RadioButton radioButtonMen;
+        private System.Windows.Forms.RadioButton radioButtonWomen;
+        private System.Windows.Forms.BindingSource BindingSourceRole;
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ЛР1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.Label = new System.Windows.Forms.Label();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(12, 68);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(776, 298);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label.Location = new System.Drawing.Point(272, 23);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(254, 25);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "Таблица пользователей";
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonAdd.Location = new System.Drawing.Point(319, 384);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(170, 39);
+            this.ButtonAdd.TabIndex = 2;
+            this.ButtonAdd.Text = "Добавить";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.DataGridView);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button ButtonAdd;
+    }
+}
+
